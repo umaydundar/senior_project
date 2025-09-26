d760cd7f82f3d120ec5e08519b712b14636ba1d4
umaydundar <umay.dundar@ug.bilkent.edu.tr>
2025-09-26 16:46:03 +0300

    some initial additions

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// Init animations
 AOS.init({
   duration: 1000,
   once: true
+});
+
+// Smooth scroll for navbar links
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
 });
